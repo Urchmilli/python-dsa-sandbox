e9113042760316d8e9180ce5640ba5c19c8c9942
Uchechukwu Okechukwu <ok.uchechukwu@gmail.com>
2024-06-21 03:15:19 -0500

    still implementing some varient of linkedlist

--- linked_list/linked_list.py
+++ linked_list/linked_list.py
@@ -1,15 +1,18 @@
-class Node:
-    def __init__(self, data=None, next=None):
-        self.data = data
-        self.next = next
+
 
 class LinkedList:
+    class Node:
+        def __init__(self, data=None, next=None):
+            self.data = data
+            self.next = next
+
     def __init__(self):
         self.head = None
 
     def insert_at_beginning(self, data):
-        node = Node(data, self.head)
+        node = self.Node(data, self.head)
         self.head = node
+        return
 
 
     def print(self):
@@ -28,21 +31,21 @@ class LinkedList:
 
     def insert_at_end(self, data):
         if self.head is None:
-            self.head = Node(data, None)
+            self.head = self.Node(data, None)
             return
-        
+
         itr = self.head
         while itr.next:
             itr = itr.next
 
-        itr.next = Node(data, None)
-
+        itr.next = self.Node(data, None)
+    #
     def insert_value(self, data_list):
         self.head = None
         for data in data_list:
             self.insert_at_end(data)
-
-
+    #
+    #
     def get_length(self):
         count = 0
         itr = self.head
@@ -51,61 +54,106 @@ class LinkedList:
             itr = itr.next
 
         return count
-    
+    #
     def remove_at(self, index):
         if index < 0 or index >= self.get_length():
             raise Exception("Invalid Index")
-        
+
         if index == 0:
             self.head = self.head.next
-            return 
-        
+            return
+
         count = 0
         itr = self.head
-        while itr: 
+        while itr:
             if count == index - 1:
                 itr.next = itr.next.next
                 break
 
             itr = itr.next
             count += 1
-
+    #
     def insert_at(self, index, data):
         if index <0 or index>self.get_length():
             raise Exception("Invalid Index")
-        
+
         if index==0:
             self.insert_at_beginning(data)
-            return 
-        
+            return
+
         count = 0
         itr = self.head
         while itr:
             if count == index - 1:
-                node = Node(data, itr.next)
+                node = self.Node(data, itr.next)
                 itr.next = node
+                break
+            itr = itr.next
+            count +=1
+
+    def insert_after_value(self, data_after, data_to_insert):
+        if self.head is None:
+            return
+
+        if self.head.data == data_after:
+            self.head.next = self.Node(data_to_insert, self.head.next)
+            return
+
+        itr = self.head
+        while itr:
+            if itr.data == data_after:
+                itr.next = self.Node(data_to_insert, itr.next)
+                break
 
             itr = itr.next
-            count +=1 
+
+    def remove_by_value(self, data):
+        if self.head is None:
+            return
+
+        if self.head.data == data:
+            self.head = self.head.next
+            return
+
+        itr = self.head
+        while itr.next:
+            if itr.next.data == data:
+                itr.next = itr.next.next
+                break
+            itr = itr.next
+
+
+
 
 
 
     
 if __name__ == "__main__":
     ll = LinkedList()
-    # ll.insert_at_beginning(5) 
+    # ll.insert_at_beginning(17)
     # ll.insert_at_beginning(89)
+    # ll.insert_at_beginning(8909)
+    #
     # ll.print()
-    # ll.insert_at_end(79)
-    # ll.insert_at_end(1)
-    # ll.insert_at_end(9876)
-    ll.insert_value(["Banana", "Mango", "Orange", "Grapes"])
+    ll.insert_value(["banana", "mango", "grapes", "orange"])
     ll.print()
-    # print("Length: ", ll.get_length())
-    ll.remove_at(2)
-    ll.print()
-    # ll.remove_at(20)
-    ll.insert_at(0, "figs")
+    # ll.insert_after_value("mango", "apple")
+    # ll.print()
+    # ll.remove_by_value("orange")
+    # ll.print()
+    # ll.remove_by_value("figs")
+    # ll.print()
+    # ll.remove_by_value("banana")
+    # ll.remove_by_value("mango")
+    # ll.remove_by_value("apple")
+    # ll.remove_by_value("grapes")
+    # ll.print()
+    print("Length: ", ll.get_length())
+    # ll.remove_at(2)
+    # ll.print()
+    # # ll.remove_at(20)
+    ll.insert_at(4, "figs")
     ll.insert_at(3, "Apple")
+    ll.insert_at(2, "Jackfruit")
     ll.print()
-    ll.print()
+    # ll.print()
